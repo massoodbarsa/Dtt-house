9900b20aa1292b04f15b8275adb619f269f6ecf3
Amir Shakiba <amir.shakiba1980@gmail.com>
2025-09-02 12:59:19 +0200

    back btn style

--- src/components/BackButton.vue
+++ src/components/BackButton.vue
@@ -15,24 +15,16 @@ const goBack = () => {
 
 <template>
   <section style="display: flex; align-items: center; gap: 20px; padding: 10px">
-    <img
-      src="/ic_back_grey@3x.png"
-      alt="Back"
-      style="width: 30px; height: 30px; padding-top: 1px; cursor: pointer"
-      @click="goBack"
-    />
-    <h3 class="back-text">{{ text }}</h3>
+    <img src="/ic_back_grey@3x.png" alt="Back" class="image" @click="goBack" />
+    <p class="back-button-label">{{ text }}</p>
   </section>
 </template>
 
 <style scoped>
-h3 {
-  margin: 0;
-  font-size: 18px;
-}
-@media (max-width: 768px) {
-  .back-text {
-    display: none;
-  }
+.image {
+  width: 30px;
+  height: 30px;
+  padding-top: 1px;
+  cursor: pointer;
 }
 </style>
